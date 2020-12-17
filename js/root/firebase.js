4c7a87766be1e67c6dd4c224057a6d4619c966e4
Marconi Mendes Marques <32400805+MarconiMendesMarques@users.noreply.github.com>
2020-12-16 21:47:59 -0300

    index e login

---
+++ js/root/firebase.js
@@ -0,0 +1,14 @@
+(function(){
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyC4TUIsQfG_n_c8ydbE4m1WAmaU2qEM_K8",
+        authDomain: "tavie-e3a34.firebaseapp.com",
+        projectId: "tavie-e3a34",
+        storageBucket: "tavie-e3a34.appspot.com",
+        messagingSenderId: "1049969921885",
+        appId: "1:1049969921885:web:38fe1bf0480b3bffdb22b4"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+    
+})()
---
+++ js/root/login.js
@@ -0,0 +1,113 @@
+var email = document.getElementById('inputEmail'); 
+var senha = document.getElementById('inputSenha');
+var authEmailPassButton = document.getElementById('authEmailPassButton');
+var createUserButton = document.getElementById('createUserButton');
+var logOutButton = document.getElementById('logOutButton');
+var displayName = document.getElementById('displayName');
+
+var id = '';
+var refUser = firebase.database().ref('users'); 
+
+if(firebase.auth().currentUser != null ){
+    firebase.auth().signOut(); 
+}; 
+
+// login com email e password
+authEmailPassButton.addEventListener('click', function(){
+
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(email.value,senha.value)
+        .then(function(){
+            id = firebase.auth().currentUser.uid 
+            alert(`bem vindo ${email.value} - id ${id}`);
+            displayName.innerText = 'Bem vindo, ' + email.value;
+            email.value = "" ;
+            senha.value = "" ; 
+           // window.location.replace("./index.html");
+        })
+        .then(function(){
+
+            refUser.orderByChild("id").equalTo(`${id}`).once("value")
+            .then(function(snapshot) {
+                snapshot.forEach(function(childSnapshot) {
+                var key = childSnapshot.key;
+                var childData = childSnapshot.val().admin;
+                console.log(childData) ; 
+                console.log(key) ; 
+                console.log(childSnapshot.val().nome) ; 
+               
+                });
+            })
+
+        })
+        .catch(function(error){
+            console.error(error.code);
+            alert('Usuário nāo cadastrado...');         
+            document.location.reload(true);
+        });
+});
+
+// Criar novo usuário
+createUserButton.addEventListener('click', function () {
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(email.value, senha.value)
+        .then(function (result) {
+            id = firebase.auth().currentUser.uid ; 
+            displayName.innerText = 'Bem vindo, ' + email.value;
+            alert(`Bem vindo ${email.value} id: ${id}`);
+
+        })
+        .catch(function (error) {
+            console.error(error.code);
+            console.error(error.message);
+            alert('Falha ao cadastrar, verifique o erro no console.')
+        });
+});
+
+// Logout
+logOutButton.addEventListener('click', function () {
+    firebase
+        .auth()
+        .signOut()
+        .then(function () {
+            displayName.innerText = 'Você não está autenticado';
+            alert('Você se deslogou');
+            email.value = "" ;
+            senha.value = "" ; 
+        }, function (error) {
+            console.error(error);
+        });
+});
+
+
+// modelo de usuario para inserir novo usuario no bd 
+
+let usuario = {
+    "id":"fulano",
+    "email":"email.teste@gmail.com",
+    "senha":"senhaemail",
+    "nome": "fulano adm", 
+    "admin": true,
+    "cpf":"266.999.888.999-99",
+    "rg": "878887888",
+    "cnpj": "89909888/0001-03",
+    "ie": "9090/0009988",
+    "enderereco":"shcgn 709 bloco c 840",
+    "bairro": "asa norte",
+    "cidade": "brasilia",
+    "estado": "df",
+    "cep": "89999-999",
+    "foneComercial": "99983-9999",
+    "foneCelular": "90900900009",
+    "redesocial": {
+        "facebook":"fjafafasdf",
+        "insta": "adfadfad",
+        "twiter": "adfasdfdas"
+    },
+    "catalogos": [
+        9876,89899,34344,56566
+    ]
+
+}
